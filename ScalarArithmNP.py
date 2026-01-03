5a138ce1de5d58b2dff8898cfb32534c7ccbdd94
Shubham Patel <shubhampatel.aids23@scet.ac.in>
2026-01-03 17:43:38 +0530

    learn vector math function

---
+++ ScalarArithmNP.py
@@ -0,0 +1,19 @@
+import numpy as np
+#4 Scalar Arithmetics
+
+# array = np.array([1,2,3])
+# print(array + 4)    
+# print(array **3) # power 3
+
+#5 vector math functions
+
+# array = np.array([1.03, 2.94, 3.021])
+# print(np.sqrt(array))
+# print(np.round(array))
+# print(np.floor(array))  
+# print(np.ceil(array))  
+
+#Exerci=se
+
+radii = np.array([1,2,3])
+print(np.pi * radii ** 2)
