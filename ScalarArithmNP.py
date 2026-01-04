fb1e6c4321d05a3afa313fb3f46f894253b5fdc3
Shubham Patel <shubhampatel.aids23@scet.ac.in>
2026-01-04 21:13:18 +0530

    Leaned Filtering

--- ScalarArithmNP.py
+++ ScalarArithmNP.py
@@ -46,15 +46,48 @@ import numpy as np
 #9 Agregate Functions :
 # Agregate Functions = summarize data and typically
 #                      return a single value
-array = np.array([[1,2,3,4,5],
-                  [6,7,8,9,10]])
+# array = np.array([[1,2,3,4,5],
+#                   [6,7,8,9,10]])
 # print(np.sum(array))        #sum of all elements
 # print(np.mean(array))       #average of all elements 
 # print(np.std(array))        #standard deviation of all elements
 # print(np.argmax(array))   #index of maximum element
 
-print(np.sum(array, axis=0))      #sum of each column
-print(np.sum(array, axis=1))      #sum of each row
-print(np.mean(array, axis=0))     #mean of each column
+# print(np.sum(array, axis=0))      #sum of each column.   0 for column
+# print(np.sum(array, axis=1))      #sum of each row       1 for row
+# print(np.mean(array, axis=0))     #mean of each column
 
-  
+# 10 Sorting
+
+# unsorted_array = np.array([3,1,4,2,5])
+# sorted_array = np.sort(unsorted_array)
+# print(sorted_array)
+# print(unsorted_array)   #original array remains unchanged
+# print(np.argsort(unsorted_array))  #indices that would sort the array
+
+# multidim array sorting
+# array = np.array([[3,1,4],
+#                   [2,5,0]])
+# # sorted_array = np.sort(array, axis=1)  #sort each row
+# # print(sorted_array)
+# # sorted_array = np.sort(array, axis=0)  #sort each column            
+# # print(sorted_array) 
+
+# print(np.argsort(array, axis=1))  #indices that would sort each row
+# print(np.argsort(array, axis=0))  #indices that would sort each column
+
+# 11 Filtering
+# filtering = refer to process of selecting elements,
+#             from array that match specific criteria
+ages = np.array([[13,53,52,23,68,97,83],
+                [15,73,92,64,18,19,17]])
+teenagers = ages[ages <18]
+adults = ages[(ages >=18) & (ages <65)]   #₹() & ()
+SeniorCitizens = ages[ages >=65]
+even = ages[ages %2 ==0]
+print(teenagers)
+print(adults)
+print(SeniorCitizens)
+print(even) 
+# print(ages[0, ages[0,:] < 18])  #first row, all columns where age <18
+# print(ages[1, ages[1,:] < 18])  #second row, all columns where age <18
