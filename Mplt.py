96406bc72c1263ef33187204a964bae5859639f1
Shubham Patel <shubhampatel.aids23@scet.ac.in>
2026-03-01 20:39:48 +0530

    Pushed Histogram

--- Mplt.py
+++ Mplt.py
@@ -99,32 +99,54 @@ import numpy as np
 # scatter Graph()
 
 
-x1 = np.array([1, 2, 3, 4, 5]) # study hour
-y1 = np.array([50,60,65,70,85]) # marks obtained
+# x1 = np.array([1, 2, 3, 4, 5]) # study hour
+# y1 = np.array([50,60,65,70,85]) # marks obtained
 
-x2 = np.array([1, 3, 6, 8, 9]) # study hour
-y2 = np.array([54,64,68,74,95]) # marks obtained
+# x2 = np.array([1, 3, 6, 8, 9]) # study hour
+# y2 = np.array([54,64,68,74,95]) # marks obtained
 
-plt.scatter(x1,y1,
-            color = 'red',
-            alpha = 0.4,          # alpha use for transparency
-            label = 'Student 1')  
+# plt.scatter(x1,y1,
+#             color = 'red',
+#             alpha = 0.4,          # alpha use for transparency
+#             label = 'Student 1')  
 
-plt.scatter(x2,y2,
-            color = 'purple',
-            alpha = 1,
-            label = 'Student 2') 
+# plt.scatter(x2,y2,
+#             color = 'purple',
+#             alpha = 1,
+#             label = 'Student 2') 
 
-plt.title('Test Score',
+# plt.title('Test Score',
+#           fontsize = 22,
+#           color = 'Orange')
+# plt.xlabel('Study Hour',
+#            fontsize = 20,
+#            color = 'red')
+# plt.ylabel('Marks Obtained',
+#            fontsize = 20,
+#            color = 'Blue')
+
+
+# plt.legend()   # legend is used to display the labels for the different data series in the plot. It helps to identify which data points belong to which series based on the color and label specified in the scatter function.
+# plt.show()
+
+
+# Histogram()  = a visual representation of distribution of quantitative data.
+
+
+scores = np.random.normal(loc = 80, scale = 10, size = 100) # Generate 1000 random scores from a normal distribution with a mean of 80 and a standard deviation of 10
+scores = np.clip(scores, 0,100) # Clip the scores to be between 0 and 100
+plt.hist(scores, bins = 12,
+         color = 'lightgreen',
+         edgecolor = 'black')
+
+
+plt.title('Histogram of Scores',
           fontsize = 22,
-          color = 'Orange')
-plt.xlabel('Study Hour',
-           fontsize = 20,
-           color = 'red')
-plt.ylabel('Marks Obtained',
-           fontsize = 20,
-           color = 'Blue')
+          color = 'darkgreen')
+plt.xlabel('Scores',
+           fontsize = 15,
+           color ='blue')
+plt.ylabel('Students',
+           fontsize =15)
 
-
-plt.legend()   # legend is used to display the labels for the different data series in the plot. It helps to identify which data points belong to which series based on the color and label specified in the scatter function.
 plt.show()
